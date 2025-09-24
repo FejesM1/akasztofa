8f8f2b052eef9d586bc95421bdbb43db83e88136
FejesM <fejesm@kkszki.hu>
2025-09-24 23:07:51 +0200

    emberke

--- akasztofa/Program.cs
+++ akasztofa/Program.cs
@@ -1,24 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace akasztofa
 {
-    
     internal class Program
     {
-        
-        
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Cyan;
-            int elet = 10;
+
+            int elet = 6; 
             string betu = "";
-            betu = betu.ToLower();
-            
+
             string[] szavak = {
                 "első","alma","kukac","kutya","macska","ház","fa","autó","iskola","könyv",
                 "asztal","szék","ablak","ajtó","utcán","tér","város","fal","kert","virág",
@@ -28,96 +21,103 @@ namespace akasztofa
                 "repülő","híd","templom","bolt","piac","posta","pénz","óra","telefon","üveg",
                 "ceruza","papír","festék","zene","dal","barát","család","anya","apa","testvér", "labubu"
             };
-            
-                string szitu = "";
-                Random rnd = new Random();
-                string szo = szavak[rnd.Next(szavak.Length)];
-                char[] kitalalnivalo = new char[szo.Length];
-                for (int i = 0; i < kitalalnivalo.Length; i++)
-                {
-                    kitalalnivalo[i] = '_';
-                }
-                Console.Title = "Akasztófa játék";
+            Random rnd = new Random();
+            string szo = szavak[rnd.Next(szavak.Length)];
 
-            
+            char[] kitalalnivalo = new char[szo.Length];
+            for (int i = 0; i < kitalalnivalo.Length; i++)
+                kitalalnivalo[i] = '_';
+
+            string szitu = "";
 
             do
-                {
+            {
+                Console.Clear();
                 Console.WriteLine("╔══════════════════════════════╗");
                 Console.WriteLine("║        AKASZTÓFA JÁTÉK       ║");
                 Console.WriteLine("╠══════════════════════════════╣");
-                    Console.WriteLine($"Élet: {elet}");
+                Console.WriteLine($"Élet: {elet}");
+                RajzolAkasztofa(elet);
                 Console.WriteLine("╚══════════════════════════════╝");
+
                 Console.WriteLine(szitu);
                 for (int i = 0; i < szo.Length; i++)
-                    {
-                        Console.Write(kitalalnivalo[i] + " ");
-                    }
+                    Console.Write(kitalalnivalo[i] + " ");
+                Console.WriteLine("\nAdjon meg egy betűt: ");
 
-                    Console.WriteLine("\nAdjon meg egy betűt: ");
-                    betu = Console.ReadLine().ToLower();
-                    
+                betu = Console.ReadLine().ToLower();
 
                 if (betu == "")
                 {
                     Console.WriteLine("Biztosan ki akarsz lépni? (i/n): ");
-                    string valasz = Console.ReadLine().ToLower();
-                    if (valasz == "i")
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        szitu = "Nem léptél ki.";
-                        continue;
-                    }
+                    if (Console.ReadLine().ToLower() == "i") break;
+                    szitu = "Nem léptél ki.";
+                    continue;
                 }
-               
+
+                if (betu.Length > 1)
+                {
+                    szitu = "Egyszerre csak egy betűt írj be.";
+                    continue;
+                }
+
                 if (szo.Contains(betu))
                 {
-                    
                     for (int i = 0; i < szo.Length; i++)
-                    {
-                        if (szo[i].ToString() == betu)
-                        {
-                            kitalalnivalo[i] = betu[0];
-                         
-                        }
-                    }
-                   
-                    szitu = "A szó tartalmazza a betűt :)\n";
-                    
-
+                        if (szo[i].ToString() == betu) kitalalnivalo[i] = betu[0];
+                    szitu = "A szó tartalmazza a betűt :)";
                 }
-                else if (betu.Length > 1)
-                {
-                     
-                    szitu = "Egyszerre csak egy betűt írj be.";
-                   
-                }
-                
                 else
                 {
                     elet--;
-                    
-                    szitu = "A szó nem tartalmazza a megadott betűt. :(\n";
-                    
+                    szitu = "A szó nem tartalmazza a betűt :(";
                 }
-                
 
                 if (elet == 0)
-                    {
-                        szitu = "Meghaltál. A kitalálandó szó {szo} volt";
-                        break;
-                    }
-                else if (!kitalalnivalo.Contains('_'))
                 {
-                    Console.Clear();    
-                    szitu = "Nyertél";
+                    Console.Clear();
+                    RajzolAkasztofa(elet);
+                    Console.WriteLine($"Meghaltál. A szó: {szo}");
                     break;
-                }Console.Clear();
-                } while (true);
+                }
+
+                if (!kitalalnivalo.Contains('_'))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Nyertél! Gratulálok!");
+                    break;
+                }
+
+            } while (true);
+        }
+
+        static void RajzolAkasztofa(int eletPont)
+        {
+            Console.WriteLine("  _______");
+            Console.WriteLine("  |     |");
+
+           
+            int hibakSzama = 6 - eletPont; 
+
+          
+            if (hibakSzama >= 1) Console.WriteLine("  |     O");
+            else Console.WriteLine("  |");
+
+           
+            if (hibakSzama >= 2) Console.WriteLine("  |     |");
+            else Console.WriteLine("  |");
+
             
+            if (hibakSzama == 3) Console.WriteLine("  |    /  ");      
+            else if (hibakSzama >= 4) Console.WriteLine("  |    /|\\ ");   
+            else Console.WriteLine("  |");
+
+           
+            if (hibakSzama == 5) Console.WriteLine("  |    /  ");      
+            else if (hibakSzama >= 6) Console.WriteLine("  |    / \\");   
+            else Console.WriteLine("  |");
+
+            Console.WriteLine("  |");
         }
     }
 }
